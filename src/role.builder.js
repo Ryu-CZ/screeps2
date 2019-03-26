2ce47624866325e3b4398bc5b877c0b140f6d6cc
Tom Trval <thandeus@gmail.com>
2019-03-26 21:55:38 +0100

    modify: search closest sources

--- src/role.builder.js
+++ src/role.builder.js
@@ -1,12 +1,3 @@
-/*
- * Module code goes here. Use 'module.exports' to export things:
- * module.exports.thing = 'a thing';
- *
- * You can import it from another modules like this:
- * var mod = require('role.builder');
- * mod.thing == 'a thing'; // true
- */
-
 var roleBuilder = {
 
     /** @param {Creep} creep **/
@@ -30,7 +21,7 @@ var roleBuilder = {
             }
 	    }
 	    else {
-	        var sources = creep.room.find(FIND_SOURCES);
+	        var sources = creep.pos.findClosestByPath(FIND_SOURCES);;
             if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(sources[0], {visualizePathStyle: {stroke: '#ffaa00'}});
             }
