6895c268d5fdba3252a930b38e03406f77408d6c
Tom Trval <thandeus@gmail.com>
2019-03-26 23:08:38 +0100

    harvester: distributes energy to where needed

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -2,13 +2,7 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < 0.38*creep.carryCapacity) {
-            var source = creep.pos.findClosestByPath(FIND_SOURCES);
-            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
-            }
-        }
-        else {
+	    if(creep.memory.delivering) ) {
             var target = creep.pos.findClosestByPath(FIND_STRUCTURES, {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
@@ -17,16 +11,30 @@ var roleHarvester = {
                     }
             });
             if(target == null ) {
+                if (creep.carry.energy < creep.carryCapacity){
+                    creep.memory.delivering = False
+                }
                 target = creep.pos.findClosestByPath(FIND_STRUCTURES)
                 if(target != null){
                     creep.moveTo(target)
                 }
             }
+            else if (creep.carry.energy == 0) {
+                creep.memory.delivering = False
+            }
             else {
                 if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
                     creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
-
+            }
+        }
+        else {
+            if (creep.carry.energy == creep.carryCapacity){
+                creep.memory.delivering = True
+            }
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
 	}
