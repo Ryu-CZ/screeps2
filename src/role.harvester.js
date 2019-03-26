be33b68f540bdd67a67382a15d765582a0ea8c99
Tom Trval <thandeus@gmail.com>
2019-03-26 22:59:07 +0100

    fix: harvester finds building

--- src/role.harvester.js
+++ src/role.harvester.js
@@ -2,7 +2,7 @@ var roleHarvester = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy < 0.1*creep.carryCapacity) {
+	    if(creep.carry.energy < 0.38*creep.carryCapacity) {
             var source = creep.pos.findClosestByPath(FIND_SOURCES);
             if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
                 creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
@@ -13,19 +13,20 @@ var roleHarvester = {
                     filter: (structure) => {
                         return (structure.structureType == STRUCTURE_EXTENSION ||
                                 structure.structureType == STRUCTURE_SPAWN ||
-                                structure.structureType == STRUCTURE_TOWER) &&  structure.energyCapacity - structure.energy > creep.carry.energy;
+                                structure.structureType == STRUCTURE_TOWER) && (structure.energyCapacity > structure.energy);
                     }
             });
-            if(!target) {
-                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
-                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
+            if(target == null ) {
+                target = creep.pos.findClosestByPath(FIND_STRUCTURES)
+                if(target != null){
+                    creep.moveTo(target)
                 }
             }
             else {
-                target = creep.pos.findClosestByPath(FIND_STRUCTURES)
-                if(target){
-                    creep.moveTo(target)
+                if(creep.transfer(target, RESOURCE_ENERGY) == ERR_NOT_IN_RANGE) {
+                    creep.moveTo(target, {visualizePathStyle: {stroke: '#ffffff'}});
                 }
+
             }
         }
 	}
