582886624463261b0a7f33cc53515c4b740fba7c
Tom Trval <thandeus@gmail.com>
2019-03-26 22:36:01 +0100

    fix: upgrader travel on empty and full

--- src/role.upgrader.js
+++ src/role.upgrader.js
@@ -2,15 +2,24 @@ var roleUpgrader = {
 
     /** @param {Creep} creep **/
     run: function(creep) {
-	    if(creep.carry.energy == 0) {
-            var sources = creep.room.find(FIND_SOURCES);
-            if(creep.harvest(sources[0]) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(sources[0]);
+        if(creep.memory.uppgrading && creep.carry.energy == 0) {
+            creep.memory.uppgrading = false;
+            creep.say('stocking energy');
+        }
+        else if (!creep.memory.uppgrading && creep.carry.energy == creep.carryCapacity) {
+            creep.memory.uppgrading = true;
+            creep.say('going uppgrading');
+        }
+
+        if(creep.memory.building) {
+            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(creep.room.controller);
             }
         }
         else {
-            if(creep.upgradeController(creep.room.controller) == ERR_NOT_IN_RANGE) {
-                creep.moveTo(creep.room.controller);
+            var source = creep.pos.findClosestByPath(FIND_SOURCES);
+            if(creep.harvest(source) == ERR_NOT_IN_RANGE) {
+                creep.moveTo(source, {visualizePathStyle: {stroke: '#ffaa00'}});
             }
         }
 	}
